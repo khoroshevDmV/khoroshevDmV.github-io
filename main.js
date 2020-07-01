5eeac48810941adbe0282bdf2b507f43c317473d
khoroshevDmV <51055754+khoroshevDmV@users.noreply.github.com>
2020-07-01 12:29:33 +0300

    version 1.1

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function show_form() {
+function show_form() {	
+	theme = document.getElementById('theme');
+	theme.style.display = 'none';
+	logo = document.getElementById('logo');
+	logo.style.display = 'none';
+	generate = document.getElementById('generate');
+	generate.style.display = 'none';
 	google_form = document.getElementById('google');
 	google_form.style.display = 'block';
 }
